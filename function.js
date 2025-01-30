7ede47973711dcc10c69648fc1cb10ed81e0cda2
Jay <careerforjay@gmail.com>
2025-01-30 00:11:37 -0500

    init commit
    just my code snippets

---
+++ function.js
@@ -0,0 +1,19 @@
+// this file is just to test and practice function basics
+// var name = "Rover";
+// var weight = 23;
+function bark(name, weight){
+    if (weight > 20) {
+        console.log(name + " says Woof! I'm a big dog!");
+    } else {
+        console.log(name + " says Woof! I'm a small dog!");
+    }
+}
+    // console.log(name + " says Woof! I weigh " + weight + " pounds.");
+
+    bark("Rover", 23);
+    bark("Spot", 13);
+    bark("Spike", 53);
+    bark("Lady", 17);
+    bark("Max", '1');
+
+// function addNumbers(a, b) {
